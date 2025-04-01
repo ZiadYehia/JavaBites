1429047abb10666cfdf3e9566a95581518735ec3
‪ziad mohamed‬‏ yehia <ziadmohamedyehia123@gmail.com>
2025-04-01 07:31:02 +0200

    ex_v1

---
+++ task1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello user!");
+        System.out.print("I am Ziad, I will be your Software developer today :)");
+/*
+        comment shortcut: ( ctrl + / )
+        Multi line comment shortcut: ( ctrl + shift + / )
+        System.out.print("username or password is not correct, please try again. ");
+*/
+
+    }
+}
